153ad3552a73964ea374c61b4f04497d1afa2a0d
Claudia <claudia365x@gmail.com>
2019-05-11 19:41:23 +0200

    Pasado 10o caso test

--- src/main/java/org/formacio/setmana1/mvc/LlibreController.java
+++ src/main/java/org/formacio/setmana1/mvc/LlibreController.java
@@ -1,12 +1,14 @@
 package org.formacio.setmana1.mvc;
 
+import org.formacio.setmana1.data.LlibreOpsBasic;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Completeu el codi per tal que la peticio /titol retorni el titol del llibre
+ * Completeu el codi per tal que la peticio /titol (@nota: /recomanacio) retorni el titol del llibre
  * amb l'isbn enviat com a parametre
  * 
  * Aquesta classe ha de fer un de LlibreOpsBasic: No poseu aqui codi de persistencia!
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LlibreController {
 
 	// Per aqui vos fara falta una referencia a un LlibreOpsBasic
+	@Autowired
+	private LlibreOpsBasic lob;
 	
 	// Aquestes anotacions i firma del metode ja son correctes
 	@RequestMapping(path="/recomanacio")
@@ -23,6 +27,6 @@ public class LlibreController {
  	public Recomanacio obteLlibre (String isbn) {
 		// Feis que retorni la recomanacio per el llibre indicat 
 		// emprat LlibreOpsBasic
-		return null; 
+		return lob.recomenacioPer(isbn); 
 	}
 }
